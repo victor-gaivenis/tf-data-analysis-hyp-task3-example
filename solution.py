2033b552b9123cf25c067b5746f3d46939c14342
victor-gaivenis <129993733+victor-gaivenis@users.noreply.github.com>
2023-04-13 22:45:07 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,12 @@
 import pandas as pd
 import numpy as np
-
+from hyppo.ksample import MMD
 
 chat_id = 987333364 # Ваш chat ID, не меняйте название переменной
 
-def solution(...) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
+def solution(x: np.array, y: np.array) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    alpha = 0.03
+    return MMD(compute_kernel="rbf", gamma=1).test(x, y)[1] < alpha # Ваш ответ, True или False
